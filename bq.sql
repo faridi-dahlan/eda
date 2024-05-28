8b1ed92d420121f8e1bc8143c3f814829b787762
Muhamad Faridi Dahlan <152169038+faridi-dahlan@users.noreply.github.com>
2024-05-28 14:46:06 +0700

    Update bq.sql

--- bq.sql
+++ bq.sql
@@ -1,5 +1,5 @@
 --eksternal table dari gcs
-CREATE OR REPLACE EXTERNAL TABLE `faridi-dezoomcamp.smartdata_da.external_netflix_titles_bq`
+CREATE OR REPLACE EXTERNAL TABLE `[project].[dataset].external_netflix_titles_bq`
 (
   show_id INT64,
   type STRING,
@@ -16,22 +16,22 @@ CREATE OR REPLACE EXTERNAL TABLE `faridi-dezoomcamp.smartdata_da.external_netfli
 )
 OPTIONS (
   format = 'CSV',
-  uris = ['gs://final_project_faridi/netflix_titles_raw.csv'],
+  uris = [uri path untuk gcs],
   skip_leading_rows = 1,
   max_bad_records = 10
 );
 
 --transformasi tabel dari tabel eksternal (lowercase dan datetype)
-CREATE OR REPLACE TABLE `faridi-dezoomcamp.smartdata_da.external_netflix_titles_bq1` AS
+CREATE OR REPLACE TABLE `[project].[dataset].external_netflix_titles_bq1` AS
 SELECT show_id, lower(type) AS type_lower, lower(title) AS title_lower, 
 lower(director) AS director_lower, lower(`cast`) AS cast_lower,lower(country) AS country_lower,
 PARSE_DATE('%B %e, %Y', date_added) as date_added,release_year,
 lower(rating) AS rating_lower,duration,lower(listed_in) AS listed_in_lower, lower(description) AS description_lower
-FROM `faridi-dezoomcamp.smartdata_da.external_netflix_titles_bq`
+FROM `[project].[dataset].external_netflix_titles_bq`
 WHERE date_added != '' AND date_added IS NOT NULL;
 
 --menyeragamkan min dan season menjadi min
-CREATE OR REPLACE TABLE `faridi-dezoomcamp.smartdata_da.external_netflix_titles_bq2` AS
+CREATE OR REPLACE TABLE `[project].[dataset].external_netflix_titles_bq2` AS
 SELECT 
   bq1.*,
   CASE
@@ -40,4 +40,4 @@ SELECT
     ELSE SAFE_CAST(SPLIT(REGEXP_REPLACE(duration, ' Seasons', ''), ' ')[OFFSET(0)] AS INT64) * 480
   END AS duration_min_filter
 FROM 
-  `faridi-dezoomcamp.smartdata_da.external_netflix_titles_bq1` AS bq1;
+  `[project].[dataset].external_netflix_titles_bq1` AS bq1;
